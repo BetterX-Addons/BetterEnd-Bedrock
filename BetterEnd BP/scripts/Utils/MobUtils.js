a51606bb06cd8244196c084b0040978adb171fc6
Halo333X <124174275+Halo333X@users.noreply.github.com>
2026-03-22 19:07:26 -0600

    Player Hit Animation Fixed

--- BetterEnd BP/scripts/Utils/MobUtils.js
+++ BetterEnd BP/scripts/Utils/MobUtils.js
@@ -1,4 +1,4 @@
-import { world, system } from "@minecraft/server";
+import { world } from "@minecraft/server";
 export default class MobUtils {
     constructor(entity) {
         this.entity = entity;
